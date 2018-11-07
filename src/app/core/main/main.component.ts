bedd57e1e526d897e7afbf05ad131f11fcd4c29f
Kingsley Victor <javaprodigy56@gmail.com>
2018-11-07 16:36:52 +0100

    Refactored homepage UI

--- src/app/core/main/main.component.ts
+++ src/app/core/main/main.component.ts
@@ -9,6 +9,14 @@ export class MainComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const fab = document.getElementById('fab');
+    const row = document.getElementById('row');
+    const sidebar = document.getElementById('sidebar');
+    fab.addEventListener('click', (event) => {
+      fab.classList.toggle('active');
+      row.classList.toggle('leftify');
+      sidebar.classList.toggle('active');
+    });
   }
 
 }
